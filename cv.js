ecab686fda36fa002e084502e34ab91fe492745d
martinevillaf <109883432+martinevillaf@users.noreply.github.com>
2023-03-24 20:47:31 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("cambiar_color_pagina").addEventListener("click", function(){
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("volver_al_color_original").addEventListener("click", function(){
+    document.body.style.backgroundColor = "white";
+});
